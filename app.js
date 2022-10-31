03740238ef811b27e904b501b1c26a2f62114cfa
Akam Programist <akadev404@gmail.com>
2022-10-31 15:16:18 +0300

    '

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-for (let index = 0; index < 10; index++) {
-  console.log(index);
-}
+const http = require("http");
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.write("hello node");
+  res.end();
+});
+console.log(`run in port http://localhost:${port}`);
+server.listen(port);
